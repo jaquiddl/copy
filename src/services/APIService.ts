c1f9b99c18bd2cd37f0eee5a4a57f1d3b0185499
jacqui <jacqui.ddl@gmail.com>
2025-04-22 22:35:05 -0600

    3 commit

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -5,7 +5,7 @@ const useApi = () => {
     const headers: HeadersInit = {
       authorization:
         "Bearer " +
-        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuLnBlcmV6IiwiQVVUSE9SSVRJRVMiOlt7ImF1dGhvcml0eSI6IlJPTEVfUkVTT1VSQ0VNQU5BR0VSIn1dLCJpYXQiOjE3NDUzNjU4MzYsImV4cCI6MTc0NTM2OTQzNn0.hXWPGDWbnICdOjytwYWg_D8yc4ailqEgVhqTiZPvdPQ",
+        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuLnBlcmV6IiwiQVVUSE9SSVRJRVMiOlt7ImF1dGhvcml0eSI6IlJPTEVfUkVTT1VSQ0VNQU5BR0VSIn1dLCJpYXQiOjE3NDUzNzA2NzgsImV4cCI6MTc0NTM3NDI3OH0.bsnulBDked000Z-jiPezo7THsTqwKeLGJ_u407BblVk",
     };
     return new Api({
       baseUrl: "http://localhost:8081",
